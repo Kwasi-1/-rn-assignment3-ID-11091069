4bb366f3c0c899bfd7f8182aa86a7cc4f987e3d9
kwasi <affulnana04@gmail.com>
2024-05-30 22:43:48 +0000

    initialised the project

---
+++ App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style= {styles.text}>
+        My name is <Text style= {styles.boldText}>Nana Kwasi</Text>
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fda',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  }
+});
